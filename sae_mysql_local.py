c6cdbe9350b932bdce070a2cdaf3ef75de2a7480
sunhughees <sunhughees@sina.cn>
2014-07-04 13:34:31 +0800

    Create sae_mysql_local.py
    this a local script to connect and work with the sae online mysql DB

---
+++ sae_mysql_local.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+# first, you should install the local environment
+# very easy, just do this:
+# pip install sae-python-dev
+
+import os
+if 'SERVER_SOFTWARE' not in os.environ:
+    from sae._restful_mysql import monkey
+    monkey.patch()
+
+# make sure type above codes before import MySQLdb
+
+import MySQLdb
+
+SAE_HOST = 'w.rdc.sae.sina.com.cn'
+SAE_PORT = 3307
+SAE_USER = '0245wl5032' # your access key
+SAE_PASSWD = '2jixxj021iy1ikm3hy3iz0wlkww0z1jy140iii0w' #  your secret key
+SAE_DB = 'app_'+'werv' # your app name 
+
+
+conn = MySQLdb.connect(host=SAE_HOST,
+                       port=3307,
+                       user=SAE_USER,
+                       passwd=SAE_PASSWD,
+                       db=SAE_DB)
+
+cur =conn.cursor()
+
+# now you can start the SQL coding
+# for example, creat a users table, and some users
+
+cur.execute('CREATE TABLE users(login VARCHAR(8), uid INT)')
+cur.execute("INSERT INTO users VALUES('john', 7000)")
+cur.execute("INSERT INTO users VALUES('jane', 7001)")
+cur.execute("INSERT INTO users VALUES('jack', 6000)")
+cur.execute("INSERT INTO users VALUES('bob', 7100)")
+
+cur.close()
+
+conn.commit() # commit all
+conn.close() # don't foget to close the connect
